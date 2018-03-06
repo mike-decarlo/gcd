012fff47650d13f89d05c8f7f663b43d971ce106
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-03-06 07:45:14 -0500

    Addition of gcd.deg() to convert from radians to degrees.

---
+++ R/gcd.deg.R
@@ -0,0 +1,20 @@
+#' Converts radians to degrees
+#' 
+#' The function \code{"gcd.deg"} takes an input of a radian value and will
+#' convert it to a degree value by the formula:
+#' \code{"degree = radian * 180 / pi"}
+#' This utility conversion is useful when dealing with geocoding coordinates,
+#' as it might be desirable to obtain the original coordinate values in
+#' degrees.
+#' 
+#' @param rad a value in radians
+#' 
+#' @examples
+#' # Input list of degree values
+#' # Longitude values range between 0 and +-180 degrees
+#' rad <- runif(1000, -180, 180) * pi / 180
+#' deg <- gcd.deg(rad)
+#' 
+#' @export
+
+gcd.deg <- function(rad) return(rad * 180 / pi)
