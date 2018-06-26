f46e6f80f4622d1a5710803b34068ad735221ef1
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-06-26 09:16:27 -0400

    made order of arguments consistent with the three functions contained within

--- R/gcd.R
+++ R/gcd.R
@@ -7,10 +7,10 @@
 #' miles (\code{"km = FALSE"}). The output is three distances using the methods
 #' of Spherical Law of Cosines, Haversine formula, and Vincenty inverse formula
 #' for ellipsoids.
-#' @param lon1 the longitude as radians of the first point
 #' @param lat1 the latitude as radians of the first point
-#' @param lon2 the longitude as radians of the second point
+#' @param lon1 the longitude as radians of the first point
 #' @param lat2 the latitude as radians of the second point
+#' @param lon2 the longitude as radians of the second point
 #' @param type defaults to "deg", can also be "rad"
 #' @param km boolean argument for whether to return results as km (TRUE) or
 #'   miles (FALSE)
@@ -28,7 +28,7 @@
 #' gcd.mi <- gcd(lon1 = deg.lon[1:500], lat1 = deg.lat[1:500]
 #'   , lon2 = deg.lon[501:1000], lat2 = deg.lat[501:1000], km = FALSE)
 
-gcd <- function(lon1, lat1, lon2, lat2, type = "deg", km = TRUE) {
+gcd <- function(lat1, lon1, lat2, lon2, type = "deg", km = TRUE) {
   if (type == "deg") {
     lon1 <- gcd.rad(lon1)
     lon2 <- gcd.rad(lon2)
