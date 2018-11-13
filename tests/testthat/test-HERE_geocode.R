f97fa4b21e9375f15d64c61fdb8e5de99c3436e6
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-13 15:05:10 -0500

    trying again

--- tests/testthat/test-HERE_geocode.R
+++ tests/testthat/test-HERE_geocode.R
@@ -1,10 +1,25 @@
 context("test-HERE_geocode")
 
 test_that("Errors when 'dev' is non-boolean", {
-  expect_error(HERE_geocode(
-    address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
-    , app_id = "zX8ajlObLPCYD2kaobj6"
-    , app_code = "NvhoO2EZsmjnExdmJtgf6g"
-    , dev = "FOO"
-    , verbose = T))
+  expect_error(
+    HERE_geocode(
+      address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
+      , app_id = "zX8ajlObLPCYD2kaobj6"
+      , app_code = "NvhoO2EZsmjnExdmJtgf6g"
+      , dev = "FOO"
+      , verbose = T
+    )
+  )
+})
+
+test_that("Returns data frame of results", {
+  expect_silent(
+    HERE_geocode(
+      address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
+      , app_id = "zX8ajlObLPCYD2kaobj6"
+      , app_code = "NvhoO2EZsmjnExdmJtgf6g"
+      , dev = F
+      , verbose = F
+    )
+  )
 })
