3fbab10ea83abb2f1c9e207eeccc74ca11525174
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-13 13:56:23 -0500

    moved inside of expectation

--- tests/testthat/test-HERE_geocode.R
+++ tests/testthat/test-HERE_geocode.R
@@ -10,12 +10,11 @@ test_that("Errors when 'dev' is non-boolean", {
 })
 
 test_that("Returns dataframe of results", {
-  df <- HERE_geocode(
+  expect_is(HERE_geocode(
     address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
     , app_id = "zX8ajlObLPCYD2kaobj6"
     , app_code = "NvhoO2EZsmjnExdmJtgf6g"
     , dev = F
     , verbose = T
-  )
-  expect_is(df, "data.frame")
+  ), "data.frame")
 })
