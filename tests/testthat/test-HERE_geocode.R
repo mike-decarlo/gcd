b2cc654ae9c516f2edde3c03626a1722241e19d5
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-13 13:31:21 -0500

    testing for HERE_geocode

---
+++ tests/testthat/test-HERE_geocode.R
@@ -0,0 +1,11 @@
+context("test-HERE_geocode")
+
+test_that("Errors when 'dev' is non-boolean", {
+  wh <- "1600 Pennsylvania Ave NW, Washington, DC 20500"
+  expect_error(HERE_geocode(
+    address = wh
+    , app_id = "zX8ajlObLPCYD2kaobj6"
+    , app_code = "NvhoO2EZsmjnExdmJtgf6g"
+    , dev = "FOO"
+    , verbose = T))
+})
