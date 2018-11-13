899d71d075e719005b921c1ce39c7af8300fb54e
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-13 15:10:22 -0500

    rollback again

--- tests/testthat/test-HERE_geocode.R
+++ tests/testthat/test-HERE_geocode.R
@@ -11,15 +11,3 @@ test_that("Errors when 'dev' is non-boolean", {
     )
   )
 })
-
-test_that("Returns data frame of results", {
-  expect_silent(
-    HERE_geocode(
-      address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
-      , app_id = "zX8ajlObLPCYD2kaobj6"
-      , app_code = "NvhoO2EZsmjnExdmJtgf6g"
-      , dev = F
-      , verbose = F
-    )
-  )
-})
