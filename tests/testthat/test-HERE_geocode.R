bd735b9be633ed3d02dffe5c3b6da39c79f70d86
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-13 13:52:51 -0500

    trying again, moved addressed as string inside function

--- tests/testthat/test-HERE_geocode.R
+++ tests/testthat/test-HERE_geocode.R
@@ -1,9 +1,8 @@
 context("test-HERE_geocode")
 
 test_that("Errors when 'dev' is non-boolean", {
-  wh <- "1600 Pennsylvania Ave NW, Washington, DC 20500"
   expect_error(HERE_geocode(
-    address = wh
+    address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
     , app_id = "zX8ajlObLPCYD2kaobj6"
     , app_code = "NvhoO2EZsmjnExdmJtgf6g"
     , dev = "FOO"
@@ -11,12 +10,12 @@ test_that("Errors when 'dev' is non-boolean", {
 })
 
 test_that("Returns dataframe of results", {
-  wh <- "1600 Pennsylvania Ave NW, Washington, DC 20500"
   df <- HERE_geocode(
-    address = wh
+    address = "1600 Pennsylvania Ave NW, Washington, DC 20500"
     , app_id = "zX8ajlObLPCYD2kaobj6"
     , app_code = "NvhoO2EZsmjnExdmJtgf6g"
     , dev = F
-    , verbose = F)
+    , verbose = T
+  )
   expect_is(df, "data.frame")
 })
