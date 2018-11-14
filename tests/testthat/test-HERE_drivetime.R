723d340e6cc5b7c66943c288d5a17f7824489b14
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-14 06:34:05 -0500

    testing for HERE_drivetime

---
+++ tests/testthat/test-HERE_drivetime.R
@@ -0,0 +1,21 @@
+context("test-HERE_drivetime")
+
+test_that("Errors when 'dev' is non-boolean", {
+  expect_error(
+    HERE_drivetime(
+      orgn_lat = 38.89875
+      , orgn_lon = -77.03653
+      , dest_lat = 38.88349
+      , dest_lon = -77.00588
+      , app_id = "zX8ajlObLPCYD2kaobj6"
+      , app_code = "NvhoO2EZsmjnExdmJtgf6g"
+      , time_frmt = "minutes"
+      , type = "fastest"
+      , trnsprt = "car"
+      , trfc = "enabled"
+      , coord_typ = "deg"
+      , dev = "FOO"
+      , verbose = T
+    )
+  )
+})
