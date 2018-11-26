3c5a1af996b7f4123269812de1af41f70a8ec3bf
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-26 12:25:26 -0500

    Updates to documentation, return values, and arguments names for greater consistency across package

--- R/HERE_geocode.R
+++ R/HERE_geocode.R
@@ -8,9 +8,11 @@
 #'   }
 #'   and returns the drive time from the JSON output code. This geocoding
 #'   information from the JSON is originally returned as a dataframe.
-#' @param address the latitude coordinate for the origin location
-#' @param app_id the longitude coordinate for the origin location
-#' @param app_code the latitude coordinate for the destination location
+#' @param address an address as a stringn
+#' @param app_id the user's App ID for the HERE.com JavaScript/REST (requires
+#'   registration)
+#' @param app_code the user's App Code for the HERE.com JavaScript/REST
+#'   (requires registration)
 #' @param dev whether to use development or production site
 #' @param verbose Logical argument determining if messages are displayed.
 #' @return location information for the entered address string:
@@ -19,13 +21,8 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom RCurl curlEscape
 #' @export
-HERE_geocode <- function(
-  address
-  , app_id
-  , app_code
-  , dev = FALSE
-  , verbose = FALSE
-  ) {
+HERE_geocode <- function(address = NULL, app_id = NULL, app_code = NULL
+  , dev = FALSE, verbose = FALSE) {
   if (dev == TRUE) {
     base <- "https://geocoder.cit.api.here.com/6.2/geocode.json?"
   } else if (dev == FALSE) {
@@ -35,105 +32,49 @@ HERE_geocode <- function(
       "Error: argument 'dev' must be given value of either TRUE or FALSE.\n"
       )
   }
-  addr <- paste0("searchtext=", curlEscape(address))
+  address <- paste0("searchtext=", curlEscape(address))
   id <- paste0("&app_id=", curlEscape(app_id))
   code <- paste0("&app_code=", curlEscape(app_code))
   gen <- "&gen=8"
-  request_url <- paste0(base, addr, id, code, gen)
+  request_url <- paste0(base, address, id, code, gen)
   if (verbose == TRUE) {
     message(request_url)
   }
   json <- fromJSON(request_url, flatten = FALSE)
-  r <- address
   if (!is.null(
     json$Response$View$Result[[1]]$Location$NavigationPosition[[1]]$Latitude[[1]]
     )) {
-    r <- c(
-      r
-      , json$Response$View$Result[[1]]$Location$NavigationPosition[[1]]$Latitude[[1]]
+      return(c(
+        "original_address" =  substr(
+          curlUnescape(address)
+          , 12
+          , nchar(curlUnescape(address))
+        )
+        , "lattitude" =
+          json$Response$View$Result[[1]]$Location$NavigationPosition[[1]]$Latitude
+        , "longitude" =
+          json$Response$View$Result[[1]]$Location$NavigationPosition[[1]]$Longitude
+        , "formatted_address" =
+          json$Response$View$Result[[1]]$Location$Address$Label
+        , "street_number" =
+          json$Response$View$Result[[1]]$Location$Address$HouseNumber
+        , "route" =
+          json$Response$View$Result[[1]]$Location$Address$Street
+        , "city" =
+          json$Response$View$Result[[1]]$Location$Address$City
+        , "district" =
+          json$Response$View$Result[[1]]$Location$Address$District
+        , "county" =
+          json$Response$View$Result[[1]]$Location$Address$County
+        , "state" =
+          json$Response$View$Result[[1]]$Location$Address$State
+        , "country" =
+          json$Response$View$Result[[1]]$Location$Address$Country
+        , "postal_code" =
+          json$Response$View$Result[[1]]$Location$Address$PostalCode
+        )
       )
   } else {
-    r <- c(r, NA)
+    stop("\nQuery returned NULL results.\n")
   }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$NavigationPosition[[1]]$Longitude[[1]]
-    )) {
-    r <- c(
-      r
-      , json$Response$View$Result[[1]]$Location$NavigationPosition[[1]]$Longitude[[1]]
-      )
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$Label[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$Label[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$Country[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$Country[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$State[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$State[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$County[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$County[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$City[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$City[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$Street[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$Street[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$HouseNumber[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$HouseNumber[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  if (!is.null(
-    json$Response$View$Result[[1]]$Location$Address$PostalCode[[1]]
-    )) {
-    r <- c(r, json$Response$View$Result[[1]]$Location$Address$PostalCode[[1]])
-  } else {
-    r <- c(r, NA)
-  }
-  results <- data.frame(t(r))
-  colnames(results) <-  c(
-    "OrigAddr"
-    , "Latitude"
-    , "Longitude"
-    , "Label"
-    , "Country"
-    , "State"
-    , "County"
-    , "City"
-    , "Street"
-    , "HouseNumber"
-    , "PostalCode"
-    )
-  return(results)
 }
