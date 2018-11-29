84814fded8bf010394d5e1eddd098261fe15809e
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-29 08:02:35 -0500

    R script for roxygen about ny_sports data

---
+++ R/ny_sport-data.R
@@ -0,0 +1,17 @@
+#' Sports Teams of the NY City Area
+#'
+#' Data for the NY City Area sports teams with the team name, sport,
+#' performance location name, and performance location address.
+#'
+#' @docType data
+#'
+#' @usage data(ny_sports)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(ny_sports)
+#' ny_sports
+"ny_sports"
